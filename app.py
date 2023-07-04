62d2856227dc23df71f9b5b1e94293ab8c2ed41a
Siroguzz <74364308+Siroguzz@users.noreply.github.com>
2023-07-04 22:07:17 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from pers_teslim import LongShortTrader, Client
+import pandas as pd
+
+st.title('Crypto Trading Bot')
+
+api_key = st.sidebar.text_input("KmV4kxWv9JpFVQGd2bzcXZqCDsxDfFgLi7FbtOPt7P1Zx2x1gA6wRFlMp4lpawqs", value='', type='password')
+api_secret = st.sidebar.text_input("d1vkFk1XLedBLB4uJldr0rPVwn2eGA9DWvd0oTdl9K5nmwOAOHJcKKAtbTJZioWq", value='', type='password')
+
+# Create a Binance client
+client = Client(api_key, api_secret, testnet=True)
+
+# Get account information
+account_info = client.get_account()
+
+# Initialize the trader
+symbol = "BTCUSDT"
+interval = "1m"
+units = 0.001
+trader = LongShortTrader(client, symbol, interval, units)
+
+# Get the most recent data
+trader.get_most_recent(days=1/76)
+
+# Define the strategy
+trader.define_strategy(sma_s=10, sma_m=20, sma_l=50)
+
+# Display a line chart of the closing prices
+st.line_chart(trader.data['Close'])
+
+# Display SMA windows
+st.sidebar.text("SMA Short: " + str(trader.sma_s))
+st.sidebar.text("SMA Medium: " + str(trader.sma_m))
+st.sidebar.text("SMA Long: " + str(trader.sma_l))
+
+# Display Binance Test Net account info
+st.subheader('Binance Test Net Account Info')
+st.dataframe(pd.DataFrame.from_dict(account_info))
+
+# Display BTC/USD price info
+btc_price_info = client.get_symbol_ticker(symbol=symbol)
+st.subheader('BTC/USD Price Info')
+st.dataframe(pd.DataFrame.from_dict([btc_price_info]))
+
+# Display buy and sell signals
+buys, sells = trader.get_signals()
+buys_df = pd.DataFrame(buys, columns=['Date', 'Price'])
+sells_df = pd.DataFrame(sells, columns=['Date', 'Price'])
+st.subheader('Buy signals')
+st.dataframe(buys_df)
+st.subheader('Sell signals')
+st.dataframe(sells_df)
+
+# Calculate and display performance metrics
+trader.evaluate_performance(buys, sells)
+st.subheader('Performance Metrics')
+st.dataframe(trader.performance_metrics)
