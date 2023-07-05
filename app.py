c855e7cb0cbd8a1124b8d0ce13e324b84275b114
Siroguzz <74364308+Siroguzz@users.noreply.github.com>
2023-07-05 16:51:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,39 @@
 import streamlit as st
-from pers_teslim import LongShortTrader, Client
-import pandas as pd
+from pers_teslim.py import Trader, Long_Short_Backtester  # your_module'un yerini kodunuzun bulunduğu dosyanın adı ile değiştirin
 
-st.title('Crypto Trading Bot')
+def main():
+    st.title('Crypto Trading Bot')
+    
+    # Kullanıcıdan giriş bilgilerini alalım
+    api_key = st.text_input("aF7Sq8sM6yJfVe7sN2XjDzxLuZt45mybHOPrEvfAAahqA2hQvoElA8Bo7v8LJe08")
+    api_secret = st.text_input("RJSWSD29RszE3BMJ1xi4f2i11OXGc4Igcz7BbQcjB9PFFBSHwng4qVtXMULeltfd")
+    
+    # Trader objesi oluşturalım
+    client = Client(api_key, api_secret)
+    trader = Trader(client)
+    
+    # Kullanıcıdan hangi kripto parayla işlem yapmak istediğini soralım
+    symbol = st.text_input("Symbol", "BTCUSDT")
+    
+    # Trader'ın verilerini alalım ve bu veriyi dataframe olarak gösterelim
+    trader.get_data(symbol=symbol)
+    st.dataframe(trader.df)
+    
+    # Backtester objesi oluşturalım
+    filepath = st.text_input("bitcoin.csv")
+    backtester = Long_Short_Backtester(filepath, symbol, start, end, tc)
+    
+    # Backtester'ın verilerini alalım ve bu veriyi dataframe olarak gösterelim
+    backtester.get_data()
+    st.dataframe(backtester.data)
+    
+    # Stratejiyi test edelim ve sonuçları gösterelim
+    smas = st.text_input("Enter SMA values (comma-separated)", "10,20,30")
+    smas = [int(sma) for sma in smas.split(",")]
+    backtester.test_strategy(smas)
+    st.write(f"Performance: {backtester.perf}")
+    st.line_chart(backtester.data[['price', 'SMA_S', 'SMA_M', 'SMA_L']])
+    
+if __name__ == "__main__":
+    main()
 
-api_key = st.sidebar.text_input("njdF90HQ3oyUtHof4YG5kQGomrZFpGxOflQDvcqDrhHw854kHEUhIhm0S3xOcK6U", value='', type='password')
-api_secret = st.sidebar.text_input("QDhVMNp5ELCdYExeLy6csDyEfmMsBPqZgIBy58VXdtXke5C4C1Hsbd3jUtaULqm0oWq", value='', type='password')
-
-# Create a Binance client
-client = Client(api_key, api_secret, testnet=True)
-
-# Get account information
-account_info = client.get_account()
-
-# Initialize the trader
-symbol = "BTCUSDT"
-interval = "1m"
-units = 0.001
-trader = LongShortTrader(client, symbol, interval, units)
-
-# Get the most recent data
-trader.get_most_recent(days=1/24)
-
-# Define the strategy
-trader.define_strategy(sma_s=15, sma_m=50, sma_l=200)
-
-# Display a line chart of the closing prices
-st.line_chart(trader.data['Close'])
-
-# Display SMA windows
-st.sidebar.text("SMA Short: " + str(trader.sma_s))
-st.sidebar.text("SMA Medium: " + str(trader.sma_m))
-st.sidebar.text("SMA Long: " + str(trader.sma_l))
-
-# Display Binance Test Net account info
-st.subheader('Binance Test Net Account Info')
-st.dataframe(pd.DataFrame.from_dict(account_info))
-
-# Display BTC/USD price info
-btc_price_info = client.get_symbol_ticker(symbol=symbol)
-st.subheader('BTC/USD Price Info')
-st.dataframe(pd.DataFrame.from_dict([btc_price_info]))
-
-# Display buy and sell signals
-buys, sells = trader.get_signals()
-buys_df = pd.DataFrame(buys, columns=['Date', 'Price'])
-sells_df = pd.DataFrame(sells, columns=['Date', 'Price'])
-st.subheader('Buy signals')
-st.dataframe(buys_df)
-st.subheader('Sell signals')
-st.dataframe(sells_df)
-
-# Calculate and display performance metrics
-trader.evaluate_performance(buys, sells)
-st.subheader('Performance Metrics')
-st.dataframe(trader.performance_metrics)
